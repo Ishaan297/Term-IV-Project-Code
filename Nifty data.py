ce6dd9732a56be878c44195cb812df3e1da996e7
Ishaan297 <123225641+Ishaan297@users.noreply.github.com>
2024-09-15 17:06:53 +0530

    Add files via upload

---
+++ Nifty data.py
@@ -0,0 +1,14 @@
+import yfinance as yf 
+import pandas as pd 
+import openpyxl as op
+start_date = "2021-01-01"
+end_date = "2024-08-23"
+
+
+closing_price = pd.DataFrame()
+extracted_data = yf.download('^NSEI',start=start_date,end=end_date)
+closing_price = extracted_data["Close"]
+closing_price.to_excel(R"C:\Users\Ishaan Naolekar\Desktop\Nifty_data.xlsx")
+
+data = pd.read_excel(R"C:\Users\Ishaan Naolekar\Desktop\Nifty_data.xlsx")
+print(data)
